5850a02585d94d6ea9bbba463bce772aa85ad07f
jedrzejasgard <j.pawlewski@asgard.gifts>
2019-10-31 12:18:49 +0100

    initial

--- src/usbcennik/urls.py
+++ src/usbcennik/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from USBproducts import views
 from .views import (home_page,
 	edycja_cen,
 	GeneratePDF)
@@ -23,6 +24,6 @@ urlpatterns = [
 	path('', home_page),
 	path('edytujceny', edycja_cen),
 	path('pdf/', GeneratePDF.as_view()),
-    path('admin/', admin.site.urls)
-    
+    path('admin/', admin.site.urls),
+    path('USB/' , include('USBproducts.urls'))
 ]
